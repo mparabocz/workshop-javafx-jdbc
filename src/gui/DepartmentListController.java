b91f7a9907e16ef79ba6bea03d0468f669ca57f6
Marcelo Parabocz <macerno@gmail.com>
2026-03-20 15:30:11 -0300

    feat: Save for Department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -46,7 +46,7 @@ public class DepartmentListController implements Initializable {
 
 	@FXML
 	public void onBtNewAction(ActionEvent event) {
-		
+
 		Stage parentStage = Utils.currentStage(event);
 		Department obj = new Department();
 		createDialogForm(obj, "/gui/DepartmentForm.fxml", parentStage);
@@ -83,11 +83,12 @@ public class DepartmentListController implements Initializable {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
-			
+
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
+			controller.setDepartmentService(new DepartmentService());
 			controller.updateFormData();
-			
+
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Enter Department Data");
 			dialogStage.setScene(new Scene(pane));
--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -5,9 +5,16 @@ import javafx.scene.Node;
 import javafx.stage.Stage;
 
 public class Utils {
-	
+
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
 
+	public static Integer tryParseToInt(String string) {
+		try {
+			return Integer.parseInt(string);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 }
